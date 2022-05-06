7083f2be42a1a459c245b2c192331a2cf87bcc55
Witek <you@example.com>
2022-05-06 23:27:16 +0200

    uwaga do enum

--- src/first/Species.java
+++ src/first/Species.java
@@ -4,6 +4,10 @@ public enum Species {
     CZLOWIEK, KRASNOLUD, OGR, TROLL, GOBLIN
 }
 
+/*
+Można też utworzyć metody dla danych typów i przypisać im statystyki bazowe dla rasy i w klasie player w tworzeniu nowej postaci statystyki beda dodawane do wartosci bazowy z enum
+*/
+
 //1.zrobić ograniczenia do podawanych wartości przy tworzeniu podstaci.
 //  Może tak jak w prawdziwym Warhammerze że każda rasa ma sjkakieś plusy i reszta losuje
 //2.zrobić polecenia z konsoli, np. zaatakuje ogra i wtedy wchodzi w pętlę walki
